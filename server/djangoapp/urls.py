9b414c82e37ceef8c1a9a7c3ce4ef0ccf454bdff
Rocky-147259 <kongrocky42@gmail.com>
2025-08-07 06:54:47 -0400

    Implemented dealer review posting with view, API call, and URL routing

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,9 +14,14 @@ urlpatterns = [
     path(route='register', view=views.registration, name='register'),
     path(route='get_cars', view=views.get_cars, name ='getcars'),
 
-
     # path for dealer reviews view
+    path('get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path('get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path('dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path('reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews'),
+
 
     # path for add a review view
+    path('add_review', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
