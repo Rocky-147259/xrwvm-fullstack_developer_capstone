4d42fa4717b2516ee96597c510dd1eb48c9d3b72
Rocky-147259 <kongrocky42@gmail.com>
2025-08-08 17:46:06 +0800

    dockerfile and other updates

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -3,7 +3,7 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# from django.utils.timezone import now
+from django.utils.timezone import now
 
 
 # Create your models here.
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,8 +3,8 @@
 import json
 import logging
 
-# from datetime import datetime
-# from django.contrib import messages
+from datetime import datetime
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
@@ -13,8 +13,8 @@ from .models import CarMake, CarModel
 from .populate import initiate
 from .restapis import analyze_review_sentiments, get_request, post_review
 
-# from django.shortcuts import get_object_or_404, redirect, render
-# from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Get an instance of a logger
